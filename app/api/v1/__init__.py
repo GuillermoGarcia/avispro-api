0e719bceb5298c3bb9a306dd811d0e8c46c43217
Guillermo García Fernández <guillermogarciaf@gmail.com>
2019-06-17 13:26:57 +0200

    Avispro Api

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,3 +3,8 @@ from flask import Blueprint
 bp = Blueprint('api', __name__)
 
 from app.api.v1 import usuarios, personajes, habilidades, combates, combatientes, errores, tokens, firebase
+
+
+import requests
+from requests.packages import urllib3
+print(urllib3.__version__)
